c076a60286d809b0bbeb23b4c659669dae04748f
Airlangga <airlanggakrishna@gmail.com>
2026-04-23 05:57:18 +0700

    feat: delete

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -117,5 +117,26 @@ func (m MovieModel) Update(movie *Movie) error {
 }
 
 func (m MovieModel) Delete(id int64) error {
+	if id < 1 {
+		return ErrRecordNotFound
+	}
+
+	result, err := m.DB.Exec(
+		`DELETE FROM movies
+		WHERE id = $1`,
+		id,
+	)
+
+	if err != nil {
+		return err
+	}
+
+	rowsAffected, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+	if rowsAffected == 0 {
+		return ErrRecordNotFound
+	}
 	return nil
 }
