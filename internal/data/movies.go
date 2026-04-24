a889ff8a6b0581a873216a664d8af7d44a6702a8
Airlangga <airlanggakrishna@gmail.com>
2026-04-24 10:11:34 +0700

    chore: limit and offset

--- internal/data/movies.go
+++ internal/data/movies.go
@@ -182,9 +182,12 @@ func (m MovieModel) GetAll(ctx context.Context, title string, genres []string, f
 		FROM movies
 		WHERE (to_tsvector('simple', title) @@ plainto_tsquery('simple', $1) OR $1 ='')
 		AND (genres @> $2 OR $2 = '{}')
-		ORDER BY %s %s, id ASC`, filters.sortColumn(), filters.sortDirection()),
+		ORDER BY %s %s, id ASC
+		LIMIT $3 OFFSET $4`, filters.sortColumn(), filters.sortDirection()),
 		title,
 		pq.Array(genres),
+		filters.limit(),
+		filters.offset(),
 	)
 
 	if err != nil {
