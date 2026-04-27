ed019b63d48d62e70b2e0a1be330f967f96e7203
Airlangga <airlanggakrishna@gmail.com>
2026-04-27 09:14:02 +0700

    chore: token

--- internal/data/tokens.go
+++ internal/data/tokens.go
@@ -1,9 +1,13 @@
 package data
 
 import (
+	"context"
 	"crypto/rand"
 	"crypto/sha256"
+	"database/sql"
 	"time"
+
+	"github.com/airlangga-hub/go-web-advanced/internal/validator"
 )
 
 const (
@@ -29,4 +33,48 @@ func generateToken(userID int64, ttl time.Duration, scope string) *Token {
 	hash := sha256.Sum256([]byte(token.Plaintext))
 	token.Hash = hash[:]
 	return token
+}
+
+func ValidateTokenPlaintext(v *validator.Validator, tokenPlaintext string) {
+	v.Check(tokenPlaintext != "", "token", "must be provided")
+	v.Check(len(tokenPlaintext) == 26, "token", "must be 26 bytes long")
+}
+
+type TokenModel struct {
+	DB *sql.DB
+}
+
+func (m TokenModel) New(userID int64, ttl time.Duration, scope string) (*Token, error) {
+	token := generateToken(userID, ttl, scope)
+	err := m.Insert(token)
+	return token, err
+}
+
+func (m TokenModel) Insert(token *Token) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(
+		ctx,
+		`INSERT INTO tokens (hash, user_id, expiry, scope)
+		VALUES ($1, $2, $3, $4)`,
+		token.Hash, token.UserID, token.Expiry, token.Scope,
+	)
+
+	return err
+}
+
+func (m TokenModel) DeleteAllForUser(scope string, userID int64) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 3*time.Second)
+	defer cancel()
+
+	_, err := m.DB.ExecContext(
+		ctx,
+		`DELETE FROM tokens
+		WHERE scope = $1
+		AND user_id = $2`,
+		scope, userID,
+	)
+
+	return err
 }
